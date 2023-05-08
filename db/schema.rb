2c2be1d51c422bfce40210c9b6c612a3bc9e2bca
Monica Wolfe <mcwolfe2011@gmail.com>
2023-05-08 13:45:58 -0700

    RSpec setup for Jungle and test using widget.

--- db/schema.rb
+++ db/schema.rb
@@ -54,10 +54,6 @@ ActiveRecord::Schema.define(version: 2023_05_05_233814) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
